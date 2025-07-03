f3c0e26542c96da1ef388e1db3a07a7005c805f7
Bukamuso <bukamusosebata@gmail.com>
2025-07-03 15:30:12 +0200

    3 news pages

---
+++ python.py
@@ -0,0 +1,24 @@
+#Numeric Data
+
+num = 3
+
+print(type(num))
+
+num2 =3.14
+
+print(type(num2))
+
+# int num = 3
+# float num = 3
+# In Python, we don't declare the data type; it automatically asigns
+
+#Variables (valid names)
+
+my_variable = 10
+total_count = 0
+user = 'John'
+
+#Invalid variable names
+_second_variable = 10
+user-name = 20
+user_name = 20  #line 23 above fixed here
