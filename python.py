41072db05cc029748f608e06cba55fe0dcfad0bf
Bukamuso <bukamusosebata@gmail.com>
2025-07-03 16:27:40 +0200

    Update python.py

--- python.py
+++ python.py
@@ -65,8 +65,20 @@ print(str1 * 3)
 num = 0
 
 if num > 0:
-    print("This number is positibe")
+    print("This number is positive")
 elif num == 0:
     print("This number is zero")
 else:
     print("This number is negative")
+    
+    #This is to ask the user to input variables and values
+    
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    
+    if num1 > num2:
+        print(num1, "is greater than" , num2)
+    elif num2 > num1:
+        print(num2, "is greater than", num1)
+    else:
+        print("Both numbers are equal")    
